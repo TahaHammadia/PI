7970697090b92523072d51d2d58382044c1eaa52
TahaHammadia <75761263+TahaHammadia@users.noreply.github.com>
2022-01-15 14:48:50 +0100

    up

--- Task11.py
+++ Task11.py
@@ -1,32 +1,5 @@
-# You can install pulp using the command: pip install -U pulp
-
+import numpy as np
 from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
-
-# Create the model
-model = LpProblem(name="small-problem", sense=LpMaximize)
-
-# Initialize the decision variables
-x = LpVariable(name="x", lowBound=0, cat="Integer")
-y = LpVariable(name="y", lowBound=0, cat="Integer")
-
-constraint = 2 * x + 4 * y >= 8
-
-model += (2 * x + y <= 20, "red_constraint")
-model += (4 * x - 5 * y >= -10, "blue_constraint")
-model += (-x + 2 * y >= -2, "yellow_constraint")
-model += (-x + 5 * y == 15, "green_constraint")
-
-# Add the objective function to the model
-model += x + 2 * y
-
-# Add the objective function to the model
-#model += lpSum([x, 2 * y])
-
-# Solve the problem
-status = model.solve()
-
-for var in model.variables():
-    print(f"{var.name}: {var.value()}")
 
 def optimize(w, p, Ws):
     """
@@ -34,4 +7,40 @@ def optimize(w, p, Ws):
     p is the list of the proportions of different groups.
     Ws is the budget of the school.
     """
-    pass
+    G = len(p)
+    model = LpProblem(name="Task_5.1", sense=LpMaximize)
+    groups = np.array([[LpVariable(name="x" + str(i), lowBound=0, cat="Integer")] for i in range(G)]) # variables
+    model += lpSum(groups) # objective function
+    print(lpSum(groups))
+
+    # Constraints
+    cost = []
+    for i in range(G):
+        cost.append(w[i,0] * groups[i, 0])
+    model += lpSum(cost) <= Ws
+    print(lpSum(cost) <= Ws)
+
+    A1 = np.array([[6 / 5 * p[i, 0]] * G for i in range(G)])
+    A2 = np.array([[-4 / 5 * p[i, 0]] * G for i in range(G)])
+    for i in range(G):
+        A1[i, i] -= 1
+        A2[i, i] += 1
+
+    set1 = A1.dot(groups)
+    set2 = A2.dot(groups)
+
+    for i in range(G):
+        model += set1[i, 0] >= 0
+        print(set1[i, 0] >= 0)
+        model += set2[i, 0] >= 0
+        print(set2[i, 0] >= 0)
+
+    status = model.solve()
+
+    for var in model.variables():
+        print(f"{var.name}: {var.value()}")
+
+w = np.array([[1], [10]])
+p = np.array([[3/4], [1/4]])
+Ws = 11
+optimize(w, p, Ws)
