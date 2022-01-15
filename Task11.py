333083a4ae1ee27a55c2667a1ed43499cb1b0568
TahaHammadia <75761263+TahaHammadia@users.noreply.github.com>
2022-01-15 15:35:16 +0100

    up

--- Task11.py
+++ Task11.py
@@ -1,25 +1,31 @@
 import numpy as np
 from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
 
-def optimize(w, p, Ws):
+def optimize(w, n, Ws):
     """
     w is the list of costs of the different groups.
-    p is the list of the proportions of different groups.
+    n is the number of candidate students from each group.
     Ws is the budget of the school.
     """
-    G = len(p)
+    G = len(n)
+    N = sum(n) # total number of all students
+    p = np.array([n[i] / N for i in range(G)])
     model = LpProblem(name="Task_5.1", sense=LpMaximize)
     groups = np.array([[LpVariable(name="x" + str(i), lowBound=0, cat="Integer")] for i in range(G)]) # variables
     model += lpSum(groups) # objective function
-    print(lpSum(groups))
 
-    # Constraints
+    # #Constraints
+    # Cost constraint
     cost = []
     for i in range(G):
         cost.append(w[i,0] * groups[i, 0])
     model += lpSum(cost) <= Ws
-    print(lpSum(cost) <= Ws)
 
+    # Number constraint
+    for i in range(G):
+        model += 1 * groups[i,0] <= n[i,0]
+
+    #4/5- rule
     A1 = np.array([[6 / 5 * p[i, 0]] * G for i in range(G)])
     A2 = np.array([[-4 / 5 * p[i, 0]] * G for i in range(G)])
     for i in range(G):
@@ -31,16 +37,15 @@ def optimize(w, p, Ws):
 
     for i in range(G):
         model += set1[i, 0] >= 0
-        print(set1[i, 0] >= 0)
         model += set2[i, 0] >= 0
-        print(set2[i, 0] >= 0)
 
+    # Solution
     status = model.solve()
 
     for var in model.variables():
         print(f"{var.name}: {var.value()}")
 
 w = np.array([[1], [10]])
-p = np.array([[3/4], [1/4]])
-Ws = 11
-optimize(w, p, Ws)
+n = np.array([[3], [1]])
+Ws = 1000
+optimize(w, n, Ws)
