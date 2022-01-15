175135be8b750b8c6a6460d3377f6aff53c3d507
TahaHammadia <75761263+TahaHammadia@users.noreply.github.com>
2022-01-15 17:17:52 +0100

    Update Task13.py

--- Task13.py
+++ Task13.py
@@ -5,3 +5,16 @@ class school13:
         studs is the list of lists of students. Each list contains the students who have ranked the school at the i^th position.
         """
         self.studs = studs
+
+class student13:
+
+    def __init__(self, group):
+        self.group = group
+
+
+def affect(n, schools):
+    """
+    n is the number of students which is supposed to be known
+    """
+
+    cpt = n # cpt counts the number of students who do not have a school yet
