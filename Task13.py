eba48c5b00c48e41863315adc199c7800c21ebb9
TahaHammadia <75761263+TahaHammadia@users.noreply.github.com>
2022-01-15 17:07:27 +0100

    Create Task13.py

---
+++ Task13.py
@@ -0,0 +1,7 @@
+class school13:
+
+    def __init__(self, studs):
+        """
+        studs is the list of lists of students. Each list contains the students who have ranked the school at the i^th position.
+        """
+        self.studs = studs
